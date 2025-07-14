06c4e140aaeab1ff44f2fc6cb1648d45932689cc
Tejasva Kant Saini <168260895+tejas0408@users.noreply.github.com>
2025-07-14 18:14:53 +0530

    added a config file

---
+++ config.js
@@ -0,0 +1,6 @@
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
+const JWT_ADMIN_PASSWORD=process.env.JWT_ADMIN_PASSWORD;
+module.exports={
+    JWT_ADMIN_PASSWORD,
+    JWT_USER_PASSWORD
+};
